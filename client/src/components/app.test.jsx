56f93e4eec49d17f1e5442e150f25437b236b932
pewiii <paulewiii3@gmail.com>
2022-06-28 00:23:32 -0400

    edit test config

--- client/src/components/app.test.jsx
+++ client/src/components/app.test.jsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App.jsx';
 
-describe('App tests', () => {
-    it('should contains the heading 1', () => {
+describe('App test', () => {
+    it('testing the test', () => {
     render(<App />);
         expect(true).toBe(true)
     });
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 //ar path = require('path');
 
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
