741ea59b1a2499fd8d4af4dd43cdc78aee26ed71
nuotian <nuot@uci.edu>
2022-07-14 00:44:41 -0400

    worked on gallery, add some overview test

--- client/src/components/Rating/Rating.test.jsx
+++ client/src/components/Rating/Rating.test.jsx
@@ -1,92 +1,92 @@
-// import { render, screen } from '@testing-library/react';
-// import React from 'react';
-// import Rating from './Rating.jsx';
-// import './rating.css';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Rating from './Rating.jsx';
+import './rating.css';
 
-// describe('Rating test', () => {
-//     it('testing the test', () => {
-//       let fakeData = [
-//         {
-//           "review_id": 5,
-//           "rating": 3,
-//           "summary": "I'm enjoying wearing these shades",
-//           "recommend": false,
-//           "response": null,
-//           "body": "Comfortable and practical.",
-//           "date": "2019-04-14T00:00:00.000Z",
-//           "reviewer_name": "shortandsweeet",
-//           "helpfulness": 5,
-//           "photos": [{
-//               "id": 1,
-//               "url": "urlplaceholder/review_5_photo_number_1.jpg"
-//             },
-//             {
-//               "id": 2,
-//               "url": "urlplaceholder/review_5_photo_number_2.jpg"
-//             },
-//           ]
-//         },
-//         {
-//           "review_id": 3,
-//           "rating": 4,
-//           "summary": "I am liking these glasses",
-//           "recommend": false,
-//           "response": "Glad you're enjoying the product!",
-//           "body": "They are very dark. But that's good because I'm in very sunny spots",
-//           "date": "2019-06-23T00:00:00.000Z",
-//           "reviewer_name": "bigbrotherbenjamin",
-//           "helpfulness": 5,
-//           "photos": [],
-//         }
-//       ]
-//       render(<Rating results ={fakeData} />);
-//       expect(
-//           screen.getByRole('heading', {
-//           name: /^2 reviews$/i
-//         })).toBeInTheDocument();
-//     });
-// });
+describe('Rating test', () => {
+    it('testing the test', () => {
+      let fakeData = [
+        {
+          "review_id": 5,
+          "rating": 3,
+          "summary": "I'm enjoying wearing these shades",
+          "recommend": false,
+          "response": null,
+          "body": "Comfortable and practical.",
+          "date": "2019-04-14T00:00:00.000Z",
+          "reviewer_name": "shortandsweeet",
+          "helpfulness": 5,
+          "photos": [{
+              "id": 1,
+              "url": "urlplaceholder/review_5_photo_number_1.jpg"
+            },
+            {
+              "id": 2,
+              "url": "urlplaceholder/review_5_photo_number_2.jpg"
+            },
+          ]
+        },
+        {
+          "review_id": 3,
+          "rating": 4,
+          "summary": "I am liking these glasses",
+          "recommend": false,
+          "response": "Glad you're enjoying the product!",
+          "body": "They are very dark. But that's good because I'm in very sunny spots",
+          "date": "2019-06-23T00:00:00.000Z",
+          "reviewer_name": "bigbrotherbenjamin",
+          "helpfulness": 5,
+          "photos": [],
+        }
+      ]
+      render(<Rating results ={fakeData} />);
+      expect(
+          screen.getByRole('heading', {
+          name: /^2 reviews$/i
+        })).toBeInTheDocument();
+    });
+});
 
 
-// describe('Rating test in ', () => {
-//   it('testing the test', () => {
-//     let fakeData = [
-//       {
-//         "review_id": 5,
-//         "rating": 3,
-//         "summary": "I'm enjoying wearing these shades",
-//         "recommend": false,
-//         "response": null,
-//         "body": "Comfortable and practical.",
-//         "date": "2019-04-14T00:00:00.000Z",
-//         "reviewer_name": "shortandsweeet",
-//         "helpfulness": 5,
-//         "photos": [{
-//             "id": 1,
-//             "url": "urlplaceholder/review_5_photo_number_1.jpg"
-//           },
-//           {
-//             "id": 2,
-//             "url": "urlplaceholder/review_5_photo_number_2.jpg"
-//           },
-//         ]
-//       },
-//       {
-//         "review_id": 3,
-//         "rating": 4,
-//         "summary": "I am liking these glasses",
-//         "recommend": false,
-//         "response": "Glad you're enjoying the product!",
-//         "body": "They are very dark. But that's good because I'm in very sunny spots",
-//         "date": "2019-06-23T00:00:00.000Z",
-//         "reviewer_name": "bigbrotherbenjamin",
-//         "helpfulness": 5,
-//         "photos": [],
-//       }
-//     ]
-//     render(<Rating results ={fakeData} />);
-//     expect(
-//       screen.getByText(/They are very dark. But that's good because I'm in very sunny spots/i)
-//        ).toContain("They are very dark. But that's good because I'm in very sunny spots");
-//   });
-// });
+describe('Rating test in ', () => {
+  it('testing the test', () => {
+    let fakeData = [
+      {
+        "review_id": 5,
+        "rating": 3,
+        "summary": "I'm enjoying wearing these shades",
+        "recommend": false,
+        "response": null,
+        "body": "Comfortable and practical.",
+        "date": "2019-04-14T00:00:00.000Z",
+        "reviewer_name": "shortandsweeet",
+        "helpfulness": 5,
+        "photos": [{
+            "id": 1,
+            "url": "urlplaceholder/review_5_photo_number_1.jpg"
+          },
+          {
+            "id": 2,
+            "url": "urlplaceholder/review_5_photo_number_2.jpg"
+          },
+        ]
+      },
+      {
+        "review_id": 3,
+        "rating": 4,
+        "summary": "I am liking these glasses",
+        "recommend": false,
+        "response": "Glad you're enjoying the product!",
+        "body": "They are very dark. But that's good because I'm in very sunny spots",
+        "date": "2019-06-23T00:00:00.000Z",
+        "reviewer_name": "bigbrotherbenjamin",
+        "helpfulness": 5,
+        "photos": [],
+      }
+    ]
+    render(<Rating results ={fakeData} />);
+    expect(
+      screen.getByText(/They are very dark. But that's good because I'm in very sunny spots/i)
+       ).toContain("They are very dark. But that's good because I'm in very sunny spots");
+  });
+});
