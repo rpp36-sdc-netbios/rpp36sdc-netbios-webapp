6d325772317ed90ae996dc3a28bfdfb578065d2a
pewiii <paulewiii3@gmail.com>
2022-07-27 16:24:36 -0400

    Remove duplicates in Related Products

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -7,9 +7,17 @@ import withInteractions from '../withInteractions.jsx';
 
 var RelatedProducts = ({ productId, changeProduct , outfit}) => {
 
+  var [products, setProducts] = useState([]);
+
   var [ data, pending, error ] = useFetch('related/' + productId);
   var outfit = [...new Set(outfit)]
 
+  useEffect(() => {
+
+    if (data) {
+      setProducts([...new Set(data)]);
+    }
+  }, [data]);
 
   return (
     <div id='related'>
@@ -18,7 +26,7 @@ var RelatedProducts = ({ productId, changeProduct , outfit}) => {
       </div>
       {error && <div>{error}</div>}
       {pending && <div>Loading...</div>}
-      {data && <SlideContainer data={data} changeProduct={changeProduct}/>}
+      <SlideContainer data={products} changeProduct={changeProduct}/>
       { outfit.length >= 1 ? <YourOutfit outfit={outfit} changeProduct={changeProduct}/> : null }
     </div>
   );
